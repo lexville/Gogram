2860d2eb50a12dc65de7eea7859484b6de7bd42e
lexville <alekikangethe@gmail.com>
2018-08-09 14:44:30 +0300

    testing out r.URL.Path

--- main.go
+++ main.go
@@ -6,7 +6,15 @@ import (
 )
 
 func handlerFunc(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "<h1>Welcome to my awesome site! </h1>")
+	w.Header().Set("Content-Type", "text/html")
+	if r.URL.Path == "/" {
+		fmt.Fprint(w, "<h1>Welcome to my awesome site! </h1>")
+	} else if r.URL.Path == "/contact" {
+		fmt.Fprint(w, "To get in touch, pleese send an email to <a href=\"mailto:support@gophergram.com\">support@gophergram.com</a>")
+	} else {
+		w.WriteHeader(http.StatusNotFound)
+		fmt.Fprint(w, "<h2>Sorry Page Not Found!!! :-(</h2>")
+	}
 }
 
 func main() {
