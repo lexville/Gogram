09acdc17923473887ecfa43492f8b6f16bb29d38
lexville <alekikangethe@gmail.com>
2018-08-10 20:28:11 +0300

    :wrench: Code refactor the view package

--- views/views.go
+++ views/views.go
@@ -1,15 +1,35 @@
 package views
 
-import "html/template"
+import (
+	"net/http"
+	"html/template"
+	"path/filepath"
+)
+
+var (
+	layoutDir = "views/layouts/"
+	templateExtension = ".gohtml"
+)
+
+// View contains a Template which is of type
+// *template.Template and the layout whisch is of
+// type string
+type View struct {
+	Template *template.Template
+	Layout   string
+}
+
+// Render is used to render the view with the predefined layout
+func (v *View) Render(w http.ResponseWriter, data interface{}) error {
+	return v.Template.ExecuteTemplate(w, v.Layout, data)
+}
 
 // NewView takes in the files needed for the view
 // and parses the files. It then returns View
 func NewView(layout string, files ...string) *View {
 	files = append(
 		files,
-		"views/layouts/footer.gohtml",
-		"views/layouts/base.gohtml",
-		"views/layouts/navbar.gohtml",
+		layoutFiles()...,
 	)
 	t, err := template.ParseFiles(files...)
 	if err != nil {
@@ -21,9 +41,13 @@ func NewView(layout string, files ...string) *View {
 	}
 }
 
-// View contains a Template which is of type
-// *template.Template
-type View struct {
-	Template *template.Template
-	Layout   string
+// layoutFiles returns a slice of strings
+// representing the layout files that will
+// be used in the application
+func layoutFiles() []string {
+	files, err := filepath.Glob(layoutDir + "*" + templateExtension)
+	if err != nil {
+		panic(err)
+	}
+	return files
 }
