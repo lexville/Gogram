da6bdb2833fba96916ff699961280d59eae20d11
lexville <alekikangethe@gmail.com>
2018-08-13 16:05:09 +0300

    exp using gorm

--- exp/main.go
+++ exp/main.go
@@ -1,11 +1,10 @@
 package main
 
 import (
-	"database/sql"
 	"fmt"
-	"log"
 
-	_ "github.com/lib/pq"
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/postgres"
 )
 
 const (
@@ -15,50 +14,36 @@ const (
 	dbname = "gogram_dev"
 )
 
+// User contains a name and email
+type User struct {
+	gorm.Model
+	Name  string
+	Email string `gorm:"type:varchar(100);unique_index"`
+}
+
 func main() {
 	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s dbname=%s sslmode=disable",
 		host, port, user, dbname)
-	db, err := sql.Open("postgres", psqlInfo)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer db.Close()
-
-	for i := 1; i <= 6; i++ {
-		userID := 1
-		if i > 3 {
-			userID = 2
-		}
-		amount := i * 100
-		description := fmt.Sprintf("USB-C Adapter x%d", i)
-		_, err = db.Exec(`
-		INSERT INTO orders(user_id, amount, description)
-		VALUES($1, $2, $3)`, userID, amount, description)
-		if err != nil {
-			panic(err)
-		}
-	}
-
-	rows, err := db.Query(`
-		SELECT *
-		FROM users
-		INNER JOIN orders on users.id=orders.user_id
-	`)
-
+	db, err := gorm.Open("postgres", psqlInfo)
 	if err != nil {
 		panic(err)
 	}
-
-	for rows.Next() {
-		var userID, orderID, amount int
-		var email, name, desc string
-		if err := rows.Scan(&userID, &name, &email, &orderID, &userID, &amount, &desc); err != nil {
-			panic(err)
-		}
-		fmt.Println("userID:", userID, "name:", name, "email:", email, "orderID:", orderID, "amount:", amount, "desc:", desc)
+	defer db.Close()
+	if err := db.DB().Ping(); err != nil {
+		panic(err)
 	}
 
-	if rows.Err() != nil {
-		panic(rows.Err())
-	}
+	db.DropTableIfExists(&User{})
+	db.LogMode(true)
+	// Migrate the schema
+	db.AutoMigrate(&User{})
+
+	// Create
+	db.Create(&User{Name: "lexville", Email: "lexville.io"})
+
+	var user User
+	db.First(&user, 1)
+	fmt.Println(user) // find product with id 1
+	db.First(&user, "email = ?", "lexville.io")
+	fmt.Println(user)
 }
